7bc1b4677d46f24eb5897246cbcaf4e25cb59f9b
Fortune Iheanacho <nachodev369@gmail.com>
2022-08-13 08:06:41 +0100

    Updated Responsive Commit

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 
 const header = document.querySelector('header');
 const navLinks = document.querySelectorAll('.navLinks li');
+const logo = document.querySelector('.mainLogo');
+const burgers = document.querySelectorAll('.bar');
 
 window.addEventListener('scroll', () => {
+
     if(this.scrollY > 100){
         header.classList.add("headerBg");
         navLinks.forEach(navLink => {
             navLink.classList.add("navChg");
         });
+        logo.src = './images/umc_logo_full_white.png';
+        burgers.forEach(bar => {
+            bar.classList.add('active')
+        })
         
     }
     else{
@@ -15,6 +22,11 @@ window.addEventListener('scroll', () => {
         navLinks.forEach(navLink => {
             navLink.classList.remove("navChg");
         });
+        logo.src = './images/umc_logo_full.png';
+
+        burgers.forEach(bar => {
+            bar.classList.remove('active')
+        })
     }
 })
 
@@ -23,6 +35,9 @@ let noOfStudents = document.querySelector('.noOfStudents');
 let noOfExams = document.querySelector('.noOfExams');
 let addNoOfStud = parseInt(noOfStudents.innerHTML);
 let addNoOfExam = parseInt(noOfExams.innerHTML);
+let accordions = document.querySelectorAll('.accordion');
+let bars = document.querySelector('.bars');
+let nav = document.querySelector('.header-flex');
 window.addEventListener('load', () => {
     
     // noOfStudents.innerHTML = 2;
@@ -35,6 +50,23 @@ window.addEventListener('load', () => {
             noOfExams.innerHTML = addNoOfExam;
         }
     }
-    setInterval(countUp, 1000);
+    setInterval(countUp, 500);
+
+    accordions.forEach(accordion => {
+        accordion.addEventListener('click', () => {
+            accordion.classList.toggle('active');
+        })
+    })
+
+    bars.addEventListener('click', () => {
+        nav.classList.toggle('active');
+
+        burgers.forEach(bar => {
+            bar.classList.toggle('closeBtn');
+        })
+    })
+
     
-})
+
+    
+});
